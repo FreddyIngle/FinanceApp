b48b4ec6667f98b403982d25e222db414a5e12fb
FrederickIngle <fingle2016@fau.edu>
2025-04-11 15:16:17 -0400

    set up tailwind css

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -8,8 +7,8 @@ function App() {
 
   return (
     <>
-      <div>
-        
+      <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">🚀 Tailwind is working!</h1>
       </div>
       
     </>
